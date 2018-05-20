f5bc92e10c81ccc91b33d46baf5945d6515d3d1f
vbssoloww <vbssoloww@gmail.com>
2018-05-20 14:56:31 +0700

    PUT faculty + controller file name

--- controllers/put_new_faculty.js
+++ controllers/put_new_faculty.js
@@ -6,29 +6,32 @@ module.exports = (req, res) => {
     let facultyId = req.params.fid;
 
     // Must match DB
-    let newDepartment = {
-        FacultyID: facultyId,
-        DepartmentID: req.body.departmentId,
-        NameEN: req.body.nameEn,
-        NameTH: req.body.nameTh,
+    let newFaculty = {
+        FacultyID:  req.body.facultyId,
+        NameEN:     req.body.nameEn,
+        NameTH:     req.body.nameTh,
+        TelNo:      req.body.telNo,
+        BelongsTo:   req.body.belongsTo,
     }
 
-    if (!newDepartment.FacultyID) {
+    if (!newFaculty.FacultyID) {
         res.status(422).send({
             "message": "Please specify faculty ID"
         })
-    }
-    if (!newDepartment.DepartmentID) {
-        res.status(422).send({
-            "message": "Please specify department ID"
-        })
-    }
-    if (!newDepartment.NameEN || !newDepartment.NameTH) {
+    } else if (!newFaculty.NameEN || !newFaculty.NameTH) {
         res.status(422).send({
             "message": "Please specify faculty names"
         })
+    } else if (!newFaculty.TelNo) {
+        res.status(422).send({
+            "message": "Please specify telephone number"
+        })
+    } else if (!newFaculty.BelongsTo) {
+        res.status(422).send({
+            "message": "Please specify faculty group"
+        })
     } else {
-        db.query(SQL.CREATE_NEW_DEPARTMENT, newDepartment, (err, results, fields) => {
+        db.query(SQL.CREATE_NEW_FACULTY, newFaculty, (err, results, fields) => {
             if (err) {
                 if (err.code === 'ER_DUP_ENTRY') {
                     res.status(400).send({
