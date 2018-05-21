885dfd8742e47cb5c41cf6f262c66704970aea85
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-05-21 14:20:05 +0700

    Added DELETE class and section

---
+++ controllers/delete_section.js
@@ -0,0 +1,43 @@
+///subjects/:subid/:year/:semester/sections/:sectno
+
+const db = require('../database')
+const SQL = require('../queries/index');
+
+module.exports = (req, res) => {
+    
+    const subjectid = req.params.subid;
+    const year = req.params.year;
+    const semester = req.params.semester;
+    const sectno = req.params.sectno;
+
+
+    if (!subjectid) {
+        res.status(422).send({
+            "message": "Please specify a Subject ID"
+        })
+    } else if (!sectno) {
+        res.status(422).send({
+            "message": "Please specify a Section Number"
+        })
+    } else {
+        db.query("DELETE FROM section WHERE SubjectID = ? AND Year = ? AND Semester = ? AND SectionNo = ?;", [subjectid, year, semester, sectno], (err, results, fields) => {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+            } else if (results.affectedRows === 0) {
+                res.status(404).send({
+                    affectedRows: results.affectedRows,
+                    message: results.message
+                });
+            } else {
+                res.status(200).send({
+                    affectedRows: results.affectedRows,
+                    message: results.message
+                });
+            }
+        })
+    }
+}
+
+
+//
