6a191f9978af7e387b87c691ee698c2bcd8bb996
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-05-22 11:33:27 +0700

    Debug GET offer typo

--- src/main/offer/controllers/get_offers.js
+++ src/main/offer/controllers/get_offers.js
@@ -2,7 +2,7 @@ const db = require('../../../database')
 const SQL = require('../../../queries/index');
 
 module.exports = (req, res) => {
-    let companyid = req.body.companyid || req.body.CompanyID || req.body.CompanyId || req.query.companyid || req.query.CompnayID || req.query.CompanyId;
+    let companyid = req.body.companyid || req.body.CompanyID || req.body.CompanyId || req.query.companyid || req.query.CompanyID || req.query.CompanyId;
     let year = req.body.year || req.body.Year || req.query.year || req.query.Year;
     
     if (!companyid) {
