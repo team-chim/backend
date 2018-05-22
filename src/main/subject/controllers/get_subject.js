f56eeec37a5388b5212ad88a04bf72a9446dc3ba
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-05-22 17:54:42 +0700

    Added subject requirements

--- src/main/subject/controllers/get_subject.js
+++ src/main/subject/controllers/get_subject.js
@@ -26,24 +26,37 @@ module.exports = (req, res) => {
                 res.sendStatus(500);
             } else {
                 if (typeof results !== 'undefined' && results.length > 0) {
-                    if (year && semester) {
-                        // Then Fetch the sections
-                        results = results[0];
-                        db.query(SQL.FIND_SUBJECT_SECTION, [subjectid, year, semester], (err, sections, fields) => {
-                            if (err) {
-                                console.log(err);
-                                res.sendStatus(500);
+                    db.query("SELECT `ReqSubjectID` FROM subject_requires WHERE `SubjectID` = ?;", subjectid, (err, requirements, fields) => {
+                        if (err) {
+                            console.log(err);
+                            res.sendStatus(500);
+                        }
+                        else {
+                            if (requirements.length > 0){
+                                results.requirements = requirements.map(e => e.ReqSubjectID);
                             } else {
-                                results.sections = sections;
-                                res.send(results);
+                                results.requirements = [];
                             }
-                        });
-                    } else {
-                        res.send({
-                            ...results,
-                            message: "Year and/or semester was not specified. Not showing section details"
-                        })
-                    }
+                            if (year && semester) {
+                                // Then Fetch the sections
+                                results = results[0];
+                                db.query(SQL.FIND_SUBJECT_SECTION, [subjectid, year, semester], (err, sections, fields) => {
+                                    if (err) {
+                                        console.log(err);
+                                        res.sendStatus(500);
+                                    } else {
+                                        results.sections = sections;
+                                        res.send(results);
+                                    }
+                                });
+                            } else {
+                                res.send({
+                                    ...results,
+                                    message: "Year and/or semester was not specified. Not showing section details"
+                                })
+                            }
+                        }
+                    })
                 } else {
                     res.status(404).send({
                         ...results,
