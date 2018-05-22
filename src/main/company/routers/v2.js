d5ea490c9a458122460a70f38a3e02a06f72ef9e
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-05-22 15:03:11 +0700

    Add company to index.html

--- src/main/company/routers/v2.js
+++ src/main/company/routers/v2.js
@@ -9,8 +9,8 @@ router.patch('/:compid', require('../controllers/patch_company'));
 router.delete('/:compid', require('../controllers/delete_company'));
 
 /* Branches */
-router.put('/:compid/branches', require('../controllers/put_new_branch'));
 router.get('/:compid/branches', require('../controllers/get_company_branches'));
+router.put('/:compid/branches', require('../controllers/put_new_branch'));
 router.get('/:compid/branches/:brid', require('../controllers/get_branch_details'));
 router.patch('/:compid/branches/:brid', require('../controllers/patch_branch_details'));
 router.delete('/:compid/branches/:brid', require('../controllers/delete_branch'));
