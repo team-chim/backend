82b0f22ff8bf27ca9c016e6431bb0b3acf03b9b3
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-05-23 16:42:18 +0700

    Debug missing payments

--- src/queries/20_payment_status.sql
+++ src/queries/20_payment_status.sql
@@ -2,6 +2,6 @@ SELECT DISTINCT t1.`StudentID`, t1.`Year`, t1.`Semester`, t1.`PaymentStatus`
 FROM (
     SELECT `StudentID`, `Year`, `Semester`, `PaymentStatus`
     FROM studies sd 
-    NATURAL JOIN payment p
+    NATURAL LEFT JOIN payment p
 ) AS t1 
 WHERE t1.studentID = ?;
